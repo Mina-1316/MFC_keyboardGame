9f71995e42583dd9707e0ed1ce3d645dccbce548
수찬 이 <tncksdl05@gmail.com>
2021-11-25 16:47:21 +0900

    종료 버튼 함수 추가

--- MFC_keyboardGameDlg.cpp
+++ MFC_keyboardGameDlg.cpp
@@ -65,6 +65,7 @@ BEGIN_MESSAGE_MAP(CMFCkeyboardGameDlg, CDialogEx)
 	ON_WM_SYSCOMMAND()
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON_EXIT, &CMFCkeyboardGameDlg::OnBnClickedButtonExit)
 END_MESSAGE_MAP()
 
 
@@ -153,3 +154,9 @@ HCURSOR CMFCkeyboardGameDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+
+
+void CMFCkeyboardGameDlg::OnBnClickedButtonExit()
+{
+	OnOK();
+}
