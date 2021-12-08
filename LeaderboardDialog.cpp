7eb5e855556bcc44e30152465801e5d2a739c210
Mina._ <kami.mina1345@gmail.com>
2021-12-08 19:59:34 +0900

    새 LeaderBoard 다이얼로그 클래스 추가

---
+++ LeaderboardDialog.cpp
@@ -0,0 +1,53 @@
+﻿// LeaderboardDialog.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "MFC_keyboardGame.h"
+#include "LeaderboardDialog.h"
+#include "afxdialogex.h"
+
+
+// LeaderboardDialog 대화 상자
+
+IMPLEMENT_DYNAMIC(LeaderboardDialog, CDialog)
+
+LeaderboardDialog::LeaderboardDialog(CWnd* pParent /*=nullptr*/)
+	: CDialog(IDD_DIALOG_LEADERBOARD, pParent)
+{
+
+}
+
+BOOL LeaderboardDialog::OnInitDialog()
+{
+	CDialog::OnInitDialog();
+
+	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
+}
+
+
+LeaderboardDialog::~LeaderboardDialog()
+{
+}
+
+void LeaderboardDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(LeaderboardDialog, CDialog)
+	ON_BN_CLICKED(IDOK, &LeaderboardDialog::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+
+// LeaderboardDialog 메시지 처리기
+
+void LeaderboardDialog::OnBnClickedOk()
+{
+	CDialog::OnOK();
+}
+
+
---
+++ LeaderboardDialog.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+
+// LeaderboardDialog 대화 상자
+
+class LeaderboardDialog : public CDialog
+{
+	DECLARE_DYNAMIC(LeaderboardDialog)
+
+	//-----변수-----
+private:
+
+
+
+public:
+	LeaderboardDialog(CWnd* pParent = nullptr);   // 표준 생성자입니다.
+	virtual ~LeaderboardDialog();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_DIALOG_LEADERBOARD };
+#endif
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+	DECLARE_MESSAGE_MAP()
+public:
+	virtual BOOL OnInitDialog();
+	afx_msg void OnBnClickedOk();
+};
