ea1dd6bd725bf806685ce1bc2aaafa83eede43ca
Mina._ <kami.mina1345@gmail.com>
2021-12-11 03:13:08 +0900

    점수 출력

--- LeaderboardDialog.h
+++ LeaderboardDialog.h
@@ -1,5 +1,6 @@
 ﻿#pragma once
 
+#include <json/json.h>
 
 // LeaderboardDialog 대화 상자
 
@@ -10,12 +11,21 @@ class LeaderboardDialog : public CDialog
 
 	//-----상수-----
 private:
-	
+	//http 통신에 필요함
+	const char* serverDomain = "http://localhost:8091/getscore";
+	const char* requestBody = "";
 
+	//그리기를 시작할 지점, 각 지점간의 갭 등을 설정
+	const int drawXPoint = 40;
+	const int drawYPoint = 20;
+	const int drawHorizonGap = 120;
+	const int drawVertGap = 15;
+
+	const int maxLeaderboardAmount = 10;
 
 	//-----변수-----
 private:
-	
+	Json::Value response;
 
 	//-----멤버 함수-----
 private:
@@ -37,4 +47,5 @@ protected:
 public:
 	virtual BOOL OnInitDialog();
 	afx_msg void OnBnClickedOk();
+	afx_msg void OnPaint();
 };
